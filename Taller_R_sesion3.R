c9b79050d3a2a5e58b3d8391c55e54d5c808e183
Gustavo Colmenares <63526540+gcolmena72@users.noreply.github.com>
2022-04-06 12:50:12 -0500

    Add files via upload

---
+++ Taller_R_sesion3.R
@@ -0,0 +1,32 @@
+
+# IMPORTAR DATOS ----------------------------------------------------------
+
+# download.file(
+#   url="https://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data",
+#               destfile = "vinos.data")
+# load(vinos.data)
+
+misDatos <- read.csv("Data_Banco.csv", header=T, sep=",")
+vinos <- read.csv("vinos.data", header=T, sep=",")
+str(misDatos)
+class(misDatos)
+head(misDatos)
+tail(misDatos)
+
+misDatos2 <- read.table("dataset_con_encabezado.txt", skip=8, header=T, sep=",")
+misDatos2
+
+misDatos3 <- read.table(
+  "https://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data", sep = ",")
+misDatos3
+head(misDatos3)
+colnames(misDatos3) <- c("cultivas", "Alcohol", "Malic acid", "Ash",
+                         "Alcalinity of ash", "Magnesium", "Total phenols",
+                         "V8","V9","V10","V11","V12", "V13")
+misDatos3
+
+download.file(
+  url="https://raw.githubusercontent.com/jboscomendoza/r-principiantes-bookdown/master/datos/breast-cancer-wis.data",
+  dest="breast-cancer-wis.data")
+misDatos4 <- read.table("breast-cancer-wis.data", header=F, sep=",")
+misDatos4
